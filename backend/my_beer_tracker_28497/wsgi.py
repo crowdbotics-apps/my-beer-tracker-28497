02b5da836901a6070616afc717884f36a5cd9261
Crowdbotics <team@crowdbotics.com>
2021-07-06 17:26:06 +0000

    Update models

--- backend/my_beer_tracker_28497/wsgi.py
+++ backend/my_beer_tracker_28497/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_beer_tracker_28497.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_beer_tracker_28497.settings")
 
 application = get_wsgi_application()
